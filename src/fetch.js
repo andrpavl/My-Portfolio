5281aebd5a1d01bdcd804407c1d46bc9d020c8b2
andrpavl <puvlo@yahoo.com>
2025-01-18 22:38:14 +0100

    fetch update

--- src/fetch.js
+++ src/fetch.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const fetchQuote = () => {
 	const resp = axios.get(
-		"https://api.api-ninjas.com/v1/quotes?category=success",
+		"https://api.api-ninjas.com/v1/quotes",
 		{
 			headers: { "X-Api-Key": "0s22dPXcN+SCoLflRiqluA==UXhoS9QLPs689AOw" },
 		}
