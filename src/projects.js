e13a64c64bdcc79b9c159bb0c6f292d66146f7fb
andrpavl <puvlo@yahoo.com>
2025-05-08 19:49:50 +0200

    add new project

--- src/projects.js
+++ src/projects.js
@@ -1,72 +1,82 @@
-import webstudioImg from "./images/webstudio.png";
-import bookshelfImg from "./images/bookshelf.png";
-import miminoImg from "./images/mimino.png";
-import moviesImg from "./images/movies.png";
-import phonebookImg from "./images/phonebook.png";
-import yogamindImg from "./images/yogamind.png";
-import todolist from "./images/todolist.png"
+import webstudioImg from './images/webstudio.png';
+import bookshelfImg from './images/bookshelf.png';
+import miminoImg from './images/mimino.png';
+import moviesImg from './images/movies.png';
+import phonebookImg from './images/phonebook.png';
+import yogamindImg from './images/yogamind.png';
+import todolist from './images/todolist.png';
+import quizImg from './images/quiz-app.png';
 
 export const projects = [
-	{
-		id: 1,
-		image: webstudioImg,
-		name: "Webstudio",
-		description: "Individual project, made during the learning HTML and CSS.  ",
-		github: "https://github.com/andrpavl/goit-markup-hw-07",
-		livepage: "https://andrpavl.github.io/goit-markup-hw-07/",
-	},
-	{
-		id: 2,
-		image: miminoImg,
-		name: "Mimino",
-		description:
-			"This is a team project. Contains an information about the restaurant and hotel with different options",
-		github: "https://github.com/andrpavl/project-team-ten",
-		livepage: "https://sshhsa.github.io/project-team-ten/",
-	},
-	{
-		id: 3,
-		image: bookshelfImg,
-		name: "BookShelf",
-		description:
-			"A team project, where I also took part. It's an online book store which gives possibility to find information about books by categories, adding/removing from/to shopping cart, finding books in different web-shops and more another options.",
-		github: "https://github.com/andrpavl/project-papyrus8",
-		livepage: "https://ii-777.github.io/project-papyrus8/",
-	},
-	{
-		id: 4,
-		image: phonebookImg,
-		name: "Phonebook",
-		description:
-			"An individual project-homework: desktop app,  where is possible to use phonebook by signing up/in, creating/deleting/sorting contacts.",
-		github: "https://github.com/andrpavl/goit-react-hw-08-phonebook",
-		livepage: "https://andrpavl.github.io/goit-react-hw-08-phonebook/",
-	},
-	{
-		id: 5,
-		image: yogamindImg,
-		name: "YogaMind",
-		description:
-			"This website is created with help of Html, CSS and a bit of JS. Made with Mobile-First princip with adaptive layout (mobile (393px), tablet (768px) and desktop (1440px) screens. This is my pet-project for training",
-		github: "https://github.com/andrpavl/YogaMind",
-		livepage: "https://andrpavl.github.io/YogaMind/",
-	},
-	{
-		id: 6,
-		image: moviesImg,
-		name: "Movies",
-		description:
-			"A simple app, which gets a list of trending movies, and has an option of searching of movies. ",
-		github: "https://github.com/andrpavl/goit-react-hw-05-movies",
-		livepage: "https://andrpavl.github.io/goit-react-hw-05-movies",
-	},
-	{
-		id: 7,
-		image: todolist,
-		name: "Todo List",
-		description:
-			"This is a training project. Adding, deleting of todos. This app is created with React, styling - TailwindCSS, database - Firebase.",
-		github: "https://github.com/andrpavl/To-do-List",
-		livepage: "https://andrpavl.github.io/To-do-List/",
-	},
+  {
+    id: 8,
+    image: quizImg,
+    name: 'Quiz Game',
+    description:
+      'A fun quiz-game, created during Javascript course in HYF. Made with HTML, CSS and Vanilla Javascript. ',
+    github: 'https://github.com/andrpavl/Quiz-game-HYF',
+    livepage: 'https://andriis-quiz.netlify.app/',
+  },
+  {
+    id: 2,
+    image: miminoImg,
+    name: 'Mimino',
+    description:
+      'This is a team project. Contains an information about the restaurant and hotel with different options',
+    github: 'https://github.com/andrpavl/project-team-ten',
+    livepage: 'https://sshhsa.github.io/project-team-ten/',
+  },
+  {
+    id: 3,
+    image: bookshelfImg,
+    name: 'BookShelf',
+    description:
+      "A team project, where I also took part. It's an online book store which gives possibility to find information about books by categories, adding/removing from/to shopping cart, finding books in different web-shops and more another options.",
+    github: 'https://github.com/andrpavl/project-papyrus8',
+    livepage: 'https://ii-777.github.io/project-papyrus8/',
+  },
+  {
+    id: 4,
+    image: phonebookImg,
+    name: 'Phonebook',
+    description:
+      'An individual project-homework: desktop app,  where is possible to use phonebook by signing up/in, creating/deleting/sorting contacts.',
+    github: 'https://github.com/andrpavl/goit-react-hw-08-phonebook',
+    livepage: 'https://andrpavl.github.io/goit-react-hw-08-phonebook/',
+  },
+  {
+    id: 5,
+    image: yogamindImg,
+    name: 'YogaMind',
+    description:
+      'This website is created with help of Html, CSS and a bit of JS. Made with Mobile-First princip with adaptive layout (mobile (393px), tablet (768px) and desktop (1440px) screens. This is my pet-project for training',
+    github: 'https://github.com/andrpavl/YogaMind',
+    livepage: 'https://andrpavl.github.io/YogaMind/',
+  },
+  {
+    id: 6,
+    image: moviesImg,
+    name: 'Movies',
+    description:
+      'A simple app, which gets a list of trending movies, and has an option of searching of movies. ',
+    github: 'https://github.com/andrpavl/goit-react-hw-05-movies',
+    livepage: 'https://andrpavl.github.io/goit-react-hw-05-movies',
+  },
+  {
+    id: 7,
+    image: todolist,
+    name: 'Todo List',
+    description:
+      'This is a training project. Adding, deleting of todos. This app is created with React, styling - TailwindCSS, database - Firebase.',
+    github: 'https://github.com/andrpavl/To-do-List',
+    livepage: 'https://andrpavl.github.io/To-do-List/',
+  },
+  {
+    id: 1,
+    image: webstudioImg,
+    name: 'Webstudio',
+    description: 'Individual project, made during the learning HTML and CSS.  ',
+    github: 'https://github.com/andrpavl/goit-markup-hw-07',
+    livepage: 'https://andrpavl.github.io/goit-markup-hw-07/',
+  },
 ];
